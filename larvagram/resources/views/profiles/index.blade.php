d7f6b8faf19ce3bf578785330c9ed48c6d247fac
subajat1 <bayumunajat@outlook.com>
2019-12-12 00:27:43 +0800

    improved profile picture and linked up posts

--- larvagram/resources/views/profiles/index.blade.php
+++ larvagram/resources/views/profiles/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="/svg/logo.svg" alt="larv_logo" width="175px">
+            <img src="/storage/{{ $user->profile->image }}" alt="larv_logo" class="w-100">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
