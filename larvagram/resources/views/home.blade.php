f38ed330f589ddb0ea6b10b09f510339e289658f
subajat1 <bayumunajat@outlook.com>
2019-12-11 16:08:18 +0800

    developed front end

--- larvagram/resources/views/home.blade.php
+++ larvagram/resources/views/home.blade.php
@@ -2,21 +2,32 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/svg/logo.svg" alt="larv_logo" width="175px">
+        </div>
+        <div class="col-9 pt-5">
+            <h1>Larvagram</h1>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">Larvagram</div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+            <div><a href="#">www.larvagram.com</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="/assets/1.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/assets/2.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/assets/3.jpg" class="w-100">
         </div>
     </div>
 </div>
--- larvagram/resources/views/layouts/app.blade.php
+++ larvagram/resources/views/layouts/app.blade.php
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="/svg/logo.svg" alt="larv" style="height: 23px;" class="pr-1"></div>
+                    <div class="pl-1 pt-1" style="border-left: 2px sold #222;">Larvagram</div> 
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
