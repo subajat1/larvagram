c7f178a9d525226ba5d99b7ad9223c9ced06ee11
subajat1 <bayumunajat@outlook.com>
2019-12-12 03:05:46 +0800

    setup welcome email for new user

---
+++ larvagram/resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Welcome to Larvagram
+
+This is an Instagram -like social media for fun and we love that you have joined us.
+
+Thanks,<br>
+Larva Admin
+@endcomponent
