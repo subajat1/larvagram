08ca6756b4a7faacbc89b75668c4471ec1c1a194
subajat1 <bayumunajat@outlook.com>
2019-12-12 03:30:54 +0800

    fixed cache parameters

--- larvagram/app/Http/Controllers/ProfilesController.php
+++ larvagram/app/Http/Controllers/ProfilesController.php
@@ -26,14 +26,14 @@ class ProfilesController extends Controller
             });
 
         $followersCount = Cache::remember(
-            'count.posts.' . $user->id, 
+            'count.followers.' . $user->id, 
             now()->addSeconds(30), 
             function() use ($user){
                 return $user->profile->followers->count();
             });
 
         $followingCount = Cache::remember(
-            'count.posts.' . $user->id, 
+            'count.following.' . $user->id, 
             now()->addSeconds(30), 
             function() use ($user){
                 return $user->following->count();
