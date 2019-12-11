34d0429488799ba5a02ff2c2cc0c0207b9f4dea9
subajat1 <bayumunajat@outlook.com>
2019-12-12 00:00:48 +0800

    developed profile policy

--- larvagram/app/Providers/AuthServiceProvider.php
+++ larvagram/app/Providers/AuthServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Profile;
+use App\Policies\ProfilePolicy;
+
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -14,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
+        Profile::class => ProfilePolicy::class,
     ];
 
     /**
--- larvagram/resources/views/profiles/index.blade.php
+++ larvagram/resources/views/profiles/index.blade.php
@@ -9,10 +9,16 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username }}</h1>
-                <div><a href="/p/create" class="btn btn-primary">POST</a></div>
+
+                @can('update', $user->profile)
+                    <div><a href="/p/create" class="btn btn-primary">POST</a></div>
+                @endcan
             </div>
-            <div><a href="/profile/{{ $user->id }}/edit">Edit Profile</a></div>
-            
+
+            @can('update', $user->profile)
+                <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+            @endcan
+
             <div class="d-flex">
                 <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
