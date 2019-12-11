5848f8c57e59a991935673c4129aaab8370c0aad
subajat1 <bayumunajat@outlook.com>
2019-12-12 01:37:10 +0800

    developed follow mechanism

--- larvagram/app/User.php
+++ larvagram/app/User.php
@@ -57,4 +57,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(Profile::class);
     }
+
+    public function following()
+    {
+        return $this->belongsToMany(Profile::class);
+    }
 }
